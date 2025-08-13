386b4db2d98b6db4d0ef197cee6b629a59097259
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2025-08-14 02:33:22 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -14,42 +14,47 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin :[ 'http://localhost:3000' ,'https://expense-tracker-pnck.vercel.app', 'https://expense-tracker-seven-lemon-62.vercel.app'  ],
+    origin: [
+        'http://localhost:3000', 
+        'https://expense-tracker-pnck.vercel.app',
+        'https://expense-tracker-seven-lemon-62.vercel.app', 
+        'https://expense-tracker-git-master-bsingh6636s-projects.vercel.app', 
+        'https://expense-tracker-ppckes54g-bsingh6636s-projects.vercel.app/'],
     credentials: true,
 }));
 app.use(express.json());
 
-app.get('/health', (req , res) => res.json('success') )
+app.get('/health', (req, res) => res.json('success'))
 
 // Routes
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/friends', friendRoutes);
-app.use('/api/category',categoryRoutes);
-app.get('/api/health', (req , res) => res.json('success') )
-app.use('/api' , (req , res) =>{
+app.use('/api/category', categoryRoutes);
+app.get('/api/health', (req, res) => res.json('success'))
+app.use('/api', (req, res) => {
     res.send('running')
-} )
+})
 
 // Health check endpoint
 app.get('/api/health', (req, res) => {
-  res.json({ message: 'Expense Tracker API is running!' });
+    res.json({ message: 'Expense Tracker API is running!' });
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
 });
 
 
 
 // Database connection and server start
 db.sequelize.authenticate().then(() => {
-  app.listen(PORT, () => {
-    logger.info(`Server is running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        logger.info(`Server is running on port ${PORT}`);
+    });
 }).catch(err => {
-  logger.error('Unable to connect to database:', err);
+    logger.error('Unable to connect to database:', err);
 });
 
 module.exports = app;
